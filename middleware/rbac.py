b08e37efba97ad125c510bdcd5e93e9dfc7b1cf3
jing <l>
2018-08-22 22:40:41 +0800

    走到哪里都能用的权限管理

---
+++ middleware/rbac.py
@@ -0,0 +1,60 @@
+from django.utils.deprecation import MiddlewareMixin
+from django.conf import settings
+from django.shortcuts import redirect,HttpResponse
+import re
+
+class RbacMiddleware(MiddlewareMixin):
+    """
+    权限控制的中间件
+    """
+
+    def process_request(self, request):
+        """
+        权限控制
+        :param request:
+        :return:
+        """
+        # 1. 获取当前请求URL
+        current_url = request.path_info
+
+        # 1.5 白名单处理
+        for reg in settings.VALID_URL:
+            if re.match(reg,current_url):
+                return None
+
+        # 2. 获取当前用户session中所有的权限
+        permission_dict = request.session.get(settings.PERMISSION_SESSION_KEY)
+        if not permission_dict:
+            return redirect('/login/')
+
+
+        request.breadcrumb_list = [
+            {'title':'首页','url':'/'},
+        ]
+
+
+        # 3. 进行权限校验
+        flag = False
+        for item in permission_dict.values():
+            id = item['id']
+            pid = item['pid']
+            pname = item['pname']
+            reg = "^%s$" % item.get('url')
+            if re.match(reg, current_url):
+                flag = True
+                if pid: # 访问的是添加客户
+                    request.current_menu_id = pid
+                    request.breadcrumb_list.extend([
+                        {'title': permission_dict[pname]['title'], 'url': permission_dict[pname]['url']},
+                        {'title': item['title'], 'url': item['url']},
+                    ])
+                else:
+                    request.current_menu_id = id
+                    request.breadcrumb_list.extend([
+                        {'title': item['title'], 'url': item['url']},
+                    ])
+
+                break
+
+        if not flag:
+            return HttpResponse('无权访问')
---
+++ service/init_permission.py
@@ -0,0 +1,56 @@
+from django.conf import settings
+
+
+def init_permission(request,user):
+    """
+    权限和菜单信息初始化，以后使用时，需要在登陆成功后调用该方法将权限和菜单信息放入session
+    :param request:
+    :param user:
+    :return:
+    """
+
+    # 3. 获取用户信息和权限信息写入session
+    permission_queryset = user.roles.filter(permissions__url__isnull=False).values('permissions__id',
+                                                                                   'permissions__url',
+                                                                                   'permissions__title',
+                                                                                   'permissions__name',
+                                                                                   'permissions__parent_id',
+                                                                                   'permissions__parent__name',
+                                                                                   'permissions__menu_id',
+                                                                                   'permissions__menu__title',
+                                                                                   'permissions__menu__icon',
+                                                                                  ).distinct()
+
+
+    menu_dict = {} # 菜单+能成为菜单的权限，用于做菜单显示
+    permission_dict = {} # 所有权限，用于做权限校验
+
+    for row in permission_queryset:
+        permission_dict[row['permissions__name']] = {
+            'id':row['permissions__id'],
+            'url': row['permissions__url'],
+            'title': row['permissions__title'],
+            'pid':row['permissions__parent_id'],
+            'pname':row['permissions__parent__name'],
+        }
+
+
+        menu_id = row.get('permissions__menu_id')
+        if not menu_id:
+            continue
+
+        if menu_id not in menu_dict:
+            menu_dict[menu_id] = {
+                'title': row['permissions__menu__title'],
+                'icon': row['permissions__menu__icon'],
+                'children': [
+                    {'id':row['permissions__id'],'title': row['permissions__title'], 'url': row['permissions__url']}
+                ]
+            }
+        else:
+            menu_dict[menu_id]['children'].append({'id':row['permissions__id'],'title': row['permissions__title'], 'url': row['permissions__url']})
+
+    print(permission_dict)
+
+    request.session[settings.PERMISSION_SESSION_KEY] = permission_dict
+    request.session[settings.MENU_SESSION_KEY] = menu_dict
